f926a793778fa1be7ed4b22c943f125d6b41c0a8
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-07-17 06:39:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Create downloads directory if it doesn't exist
 mkdir -p downloads
 
-pip install --upgrade 
+pip install --upgrade pip 
 # Install required Python packages
 pip install pyrogram moviepy
 
